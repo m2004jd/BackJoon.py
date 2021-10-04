f34517d1d91f913e75b4524a81b8c4d99eae1c3a
MJD <65656409+m2004jd@users.noreply.github.com>
2021-10-04 18:34:57 +0900

    Delete 14681.py

--- StepByStep/Lv.2/14681.py
+++
@@ -1,9 +0,0 @@
-x, y = map(int, input().split())
-if x > 0 and y > 0 :    # x: +, y: +
-    print('1')
-elif x < 0 and y > 0 :  # x: -, y: +
-    print('2')
-elif x < 0 and y < 0 :  # x: -, y: -
-    print('3')
-else:                   # x: +, y: -
-    print('4')
