068fb116c12235a09a3a6ed8d06cf933887e94ae
MJD <65656409+m2004jd@users.noreply.github.com>
2021-10-04 18:35:29 +0900

    Delete 2884.py

--- StepByStep/Lv.2/2884.py
+++
@@ -1,7 +0,0 @@
-h, m = map(int, input().split())
-if m >= 45:                 # m이 45 이상일때
-    print(h, m - 45)
-elif h > 0 and m < 45:      # m이 45 미만일때
-    print(h - 1, m + 15)
-else:                       # h이 0 일때
-    print(23, m + 15)
